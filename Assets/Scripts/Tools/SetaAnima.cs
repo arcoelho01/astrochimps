3c156d4a948bc0189cf334f0abc483c2983524ee
Leonardo Schenkel <leoschenkel@gmail.com>
2012-05-28 22:20:37 -0300

    Incluindo a seta do Freddy, materiais, animação. falta corrigir o ponto certo do loop.

---
+++ Assets/Scripts/Tools/SetaAnima.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class SetaAnima : MonoBehaviour {
+	
+	Animation Seta_ANI;
+	bool already_Started = false;
+	public GameObject Seta = null; 
+	
+	// Use this for initialization
+	void Start () {
+		
+		Seta_ANI = Seta.GetComponent<Animation>().animation; // Passa a pista completa de animação para a variável
+		Seta_ANI.Play("Grow"); // Toca a animação I (Erguendo a caixa)
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if ( !Seta_ANI.isPlaying )
+			Seta_ANI.Play("Bounce");
+	
+	}
+}
