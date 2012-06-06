e8800b41eb857b699b99eb814e759232f7d4a69d
jinay <riberodrigo@gmail.com>
2012-06-05 22:30:39 -0300

    identando direito

--- Assets/Scripts/GerenciadorSlots.cs
+++ Assets/Scripts/GerenciadorSlots.cs
@@ -17,11 +17,11 @@ public class GerenciadorSlots : MonoBehaviour {
 			canos[i] = GameObject.Find("CanoCentral"+(i+1));
 			slots[i] = GameObject.Find("Slot"+(i+1));
 			regioes[i] = GameObject.Find("Regiao"+(i+1));
-      if(i >= 3){
-  			canos[i].active = false;
-  			slots[i].active = false;
-  			regioes[i].active = false;
-      }
+	      if(i >= 3){
+	  			canos[i].active = false;
+	  			slots[i].active = false;
+	  			regioes[i].active = false;
+	      }
 		}
 		
 	}
@@ -38,9 +38,9 @@ public class GerenciadorSlots : MonoBehaviour {
 		
 		if(!alteracaoBloqueada)
 		{
-			if((slots[0].GetComponent<DefinicaoEstrutura>().statusProgressao == DefinicaoEstrutura.StatusProgresso.CONCLUIDO)&&
-				(slots[1].GetComponent<DefinicaoEstrutura>().statusProgressao == DefinicaoEstrutura.StatusProgresso.CONCLUIDO)&&
-				(slots[2].GetComponent<DefinicaoEstrutura>().statusProgressao == DefinicaoEstrutura.StatusProgresso.CONCLUIDO))
+			if((slots[0].GetComponent<DefinicaoEstrutura>().tipo != DefinicaoEstrutura.TipoEstrutura.SLOT)&&
+				(slots[1].GetComponent<DefinicaoEstrutura>().tipo != DefinicaoEstrutura.TipoEstrutura.SLOT)&&
+				(slots[2].GetComponent<DefinicaoEstrutura>().tipo != DefinicaoEstrutura.TipoEstrutura.SLOT))
 			{
 				alteracaoBloqueada = true;
 				for(int i = 3; i< 9; i++)
