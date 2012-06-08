7bbac5b015086e51b1cc29472d27338ea3b69660
Leonardo Schenkel <leoschenkel@gmail.com>
2012-06-07 23:42:02 -0300

    HUD QUASE toda Montada: Ja da para setar um nível de oxigeno (variável publica) e tb setar qual prédio ocupa qual slot. (função setSlot) Ainda nao interage e nao recebe os mouseclicks

--- Assets/Scripts/GUI/GUIControl.cs
+++ Assets/Scripts/GUI/GUIControl.cs
@@ -3,11 +3,107 @@ using System.Collections;
 
 public class GUIControl : MonoBehaviour {
 	
-	public Texture2D BoxTexture;
+	public Texture2D BoxMacacoTexture;
+	public Texture2D BoxMapaTexture;
+	
 	public Texture2D MonkeyTexture;
 	public Texture2D BarraVaziaTexture;
+	public Texture2D BoxMetalTexture;
+	public Texture2D BoxOxigenioTexture;
+	public Texture2D ReguaOxigenioTexture;
+	public Texture2D NivelOxigenioTexture;
+	
+	
+	//Macaco 1 - Astronauta
 	public Texture2D AtaqueTextureON;
 	public Texture2D MoverTextureON;
+	public Texture2D AtaqueTextureOFF;
+	public Texture2D MoverTextureOFF;
+	
+	//Macaco 2 - Engenheiro
+	public Texture2D ReciclarTextureON;
+	public Texture2D RepararTextureON;
+	public Texture2D ReciclarTextureOFF;
+	public Texture2D RepararTextureOFF;
+	
+	
+	//Macaco 3 - Sabotador
+	public Texture2D SabotarTextureON;
+	public Texture2D SabotarTextureOFF;
+	
+	//Macaco 4 - Cientista
+	public Texture2D ProcurarTextureON;
+	public Texture2D ProcurarTextureOFF;
+	
+	// FABRICA
+	public Texture2D FabricaTexture;
+	public Texture2D DroneCacadorTexture;
+	public Texture2D DroneSabotadorTexture;
+	public Texture2D DronePatrulhaTexture;
+	
+	public float NivelOxigenioFloat = 0.5f;
+	private Texture2D SlotPredio1;
+	private Texture2D SlotPredio2;
+	private Texture2D SlotPredio3;
+	private Texture2D SlotPredio4;
+	private Texture2D SlotPredio5;
+	private Texture2D SlotPredio6;
+	private Texture2D SlotPredio7;
+	private Texture2D SlotPredio8;
+	private Texture2D SlotPredio9;
+	
+	public Texture2D ComandoOxTexture;
+	public Texture2D LabOxTexture;
+	public Texture2D SegurancaOxTexture;
+	public Texture2D FazendaOxTexture;
+	public Texture2D FabricaOxTexture;
+	
+	
+	// DEFINE - tem que fazer
+	// Centro de Comando - 1
+	// Laboratorio - 2
+	// Seguranca - 3
+	// Fazenda - 4
+	// Fabricao - 5
+	public void setSlot(int slotNumber, int predio)
+	{
+		Texture2D tempPredio = null;
+		
+		if (predio == 1)
+			tempPredio =ComandoOxTexture;
+		else if(predio == 2)
+			tempPredio = LabOxTexture;
+		else if(predio == 3)
+			tempPredio = SegurancaOxTexture;
+		else if(predio == 4)
+			tempPredio = FabricaOxTexture;
+		else if(predio == 5)
+			tempPredio = FabricaOxTexture;
+		
+		if (slotNumber == 1)
+			SlotPredio1 = tempPredio;
+		if (slotNumber == 2)
+			SlotPredio2 = tempPredio;
+		if (slotNumber == 3)
+			SlotPredio3 = tempPredio;
+		if (slotNumber == 4)
+			SlotPredio4 = tempPredio;
+		if (slotNumber == 5)
+			SlotPredio5 = tempPredio;
+		if (slotNumber == 6)
+			SlotPredio6 = tempPredio;
+		if (slotNumber == 7)
+			SlotPredio7 = tempPredio;
+		if (slotNumber == 8)
+			SlotPredio8 = tempPredio;
+		if (slotNumber == 9)
+			SlotPredio9 = tempPredio;
+
+	}
+	
+	
+	//FALTA FAZER OS SLOTS DOS PREDIOS
+
 	
 	
 	void OnGUI () {
@@ -16,11 +112,35 @@ public class GUIControl : MonoBehaviour {
 		int boxYDistance = 172;
 		int box5x = 826;
 		
-		GUI.Label (new Rect (box1x,boxY,BoxTexture.width,BoxTexture.height), BoxTexture);
-		GUI.Label (new Rect (box1x + boxYDistance, boxY,BoxTexture.width,BoxTexture.height), BoxTexture);
-		GUI.Label (new Rect (box1x + boxYDistance * 2 ,boxY,BoxTexture.width,BoxTexture.height), BoxTexture);
-		GUI.Label (new Rect (box1x + boxYDistance * 3,boxY,BoxTexture.width,BoxTexture.height), BoxTexture);
-		GUI.Label (new Rect (box5x,boxY,BoxTexture.width,BoxTexture.height), BoxTexture);
+		
+		setSlot(1,1);
+		setSlot(2,2);
+		setSlot(3,4);
+		setSlot(3,3);
+		
+		
+		GUI.Label (new Rect (8,4,BoxMapaTexture.width,BoxMapaTexture.height), BoxMapaTexture);
+		
+		GUI.Label (new Rect (210,5,BoxMetalTexture.width,BoxMetalTexture.height), BoxMetalTexture);
+		GUI.Label (new Rect (380,5,BoxOxigenioTexture.width,BoxOxigenioTexture.height), BoxOxigenioTexture);
+		
+		
+		
+		for ( int x = 556 ; x < 560 + (NivelOxigenioFloat * 280); x = x +4){
+			GUI.Label (new Rect (x,16,4,38), NivelOxigenioTexture);
+		}
+			
+		GUI.Label (new Rect (550,10,ReguaOxigenioTexture.width,ReguaOxigenioTexture.height), ReguaOxigenioTexture);
+		
+		GUI.Label (new Rect (558,10,ComandoOxTexture.width,ComandoOxTexture.height), SlotPredio1);
+		GUI.Label (new Rect (587,16,ComandoOxTexture.width,ComandoOxTexture.height), SlotPredio2);
+		GUI.Label (new Rect (624,23,ComandoOxTexture.width,ComandoOxTexture.height), SlotPredio3);
+		
+		GUI.Label (new Rect (box1x,boxY,BoxMacacoTexture.width,BoxMacacoTexture.height), BoxMacacoTexture);
+		GUI.Label (new Rect (box1x + boxYDistance, boxY,BoxMacacoTexture.width,BoxMacacoTexture.height), BoxMacacoTexture);
+		GUI.Label (new Rect (box1x + boxYDistance * 2 ,boxY,BoxMacacoTexture.width,BoxMacacoTexture.height), BoxMacacoTexture);
+		GUI.Label (new Rect (box1x + boxYDistance * 3,boxY,BoxMacacoTexture.width,BoxMacacoTexture.height), BoxMacacoTexture);
+		GUI.Label (new Rect (box5x,boxY,BoxMacacoTexture.width,BoxMacacoTexture.height), BoxMacacoTexture);
 		
 		
 		GUI.Label (new Rect (box1x +15,boxY +11,MonkeyTexture.width,MonkeyTexture.height), MonkeyTexture);
@@ -35,9 +155,35 @@ public class GUIControl : MonoBehaviour {
 		GUI.Label (new Rect (box1x +79  + boxYDistance *2,barraY , BarraVaziaTexture.width,BarraVaziaTexture.height), BarraVaziaTexture);
 		GUI.Label (new Rect (box1x +79  + boxYDistance *3,barraY , BarraVaziaTexture.width,BarraVaziaTexture.height), BarraVaziaTexture);
 		
-		
+		// Macaco 1 - ASTRONAUTA
 		GUI.Label (new Rect (box1x + 18 , boxY + 71, AtaqueTextureON.width,AtaqueTextureON.height), AtaqueTextureON);
 		GUI.Label (new Rect (box1x + 18 + 45 , boxY + 71, MoverTextureON.width,MoverTextureON.height), MoverTextureON);
+		
+		// Macaco 2 - ENGENHEIRO
+		GUI.Label (new Rect (box1x + 18 +boxYDistance, boxY + 71, ReciclarTextureON.width,ReciclarTextureON.height), ReciclarTextureON);
+		GUI.Label (new Rect (box1x + 18 + 45 +boxYDistance, boxY + 71, RepararTextureON.width,RepararTextureON.height), RepararTextureON);
+		GUI.Label (new Rect (box1x + 18 + 45 +45+boxYDistance, boxY + 71, MoverTextureON.width,MoverTextureON.height), MoverTextureON);
+		
+		// Macaco 3 - ENGENHEIRO
+		GUI.Label (new Rect (box1x + 18 + boxYDistance*2, boxY + 71, SabotarTextureON.width,SabotarTextureON.height), SabotarTextureON);
+		GUI.Label (new Rect (box1x + 18 + 45 +boxYDistance*2, boxY + 71, RepararTextureON.width,RepararTextureON.height), RepararTextureON);
+		GUI.Label (new Rect (box1x + 18 + 45 + 45+boxYDistance*2, boxY + 71, MoverTextureON.width,MoverTextureON.height), MoverTextureON);
+		
+		
+		// Macaco 4 - Cientista
+		GUI.Label (new Rect (box1x + 18 + boxYDistance*3, boxY + 71, ProcurarTextureON.width,ProcurarTextureON.height), ProcurarTextureON);
+		GUI.Label (new Rect (box1x + 18 + 45+boxYDistance*3, boxY + 71, MoverTextureON.width,MoverTextureON.height), MoverTextureON);
+		
+		// Fabrica de Drones
+		GUI.Label (new Rect (box1x +15 + boxYDistance *3 ,boxY +11,MonkeyTexture.width,MonkeyTexture.height), MonkeyTexture);
+		
+		GUI.Label (new Rect (box5x + 18 , boxY + 71, DroneCacadorTexture.width,DroneCacadorTexture.height), DroneCacadorTexture);
+		GUI.Label (new Rect (box5x + 18 +45, boxY + 71, DroneSabotadorTexture.width,DroneSabotadorTexture.height), DroneSabotadorTexture);
+		GUI.Label (new Rect (box5x + 18 +45+45, boxY + 71, DronePatrulhaTexture.width,DronePatrulhaTexture.height), DronePatrulhaTexture);
+		
+		//GUI.
+		
+		
 }
 	
 	// Use this for initialization
